fa24c5a94409ef0e43f276fe07eee5b43ddb2924
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-22 20:29:52 -0300

    Update page.tsx

--- app/marcas/mercedes/page.tsx
+++ app/marcas/mercedes/page.tsx
@@ -1,1 +1,17 @@
+import BrandPage, { BrandData } from "../../../components/BrandPage";
 
+export default function Page() {
+  const data: BrandData = {
+    name: "Mercedes-Benz",
+    heroImage: "/trucks/mercedes.jpg",
+    logo: "/logos/mercedes.png",
+    specs: [
+      { label: "Motor", value: "A definir" },
+      { label: "Potência", value: "A definir" },
+      { label: "Transmissão", value: "A definir" },
+    ],
+    gallery: ["/trucks/mercedes-1.jpg", "/trucks/mercedes-2.jpg"],
+    videos: [{ title: "Mercedes Actros", url: "https://www.youtube.com/embed/VIDEO_ID" }],
+  };
+  return <BrandPage data={data} />;
+}
