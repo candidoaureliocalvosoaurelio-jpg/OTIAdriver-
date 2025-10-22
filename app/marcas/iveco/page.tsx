a90cfa442592b691e7175ba33ae695c7921b393e
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-22 20:28:04 -0300

    Update page.tsx

--- app/marcas/iveco/page.tsx
+++ app/marcas/iveco/page.tsx
@@ -1,1 +1,19 @@
+import BrandPage, { BrandData } from "../../../components/BrandPage";
 
+export default function Page() {
+  const data: BrandData = {
+    name: "Iveco",
+    heroImage: "/trucks/iveco.jpg",
+    logo: "/logos/iveco.png",
+    specs: [
+      { label: "Motor", value: "A definir" },
+      { label: "Potência", value: "A definir" },
+      { label: "Torque", value: "A definir" },
+      { label: "PBTC", value: "A definir" },
+      { label: "Transmissão", value: "A definir" },
+    ],
+    gallery: ["/trucks/iveco-1.jpg", "/trucks/iveco-2.jpg"],
+    videos: [{ title: "Apresentação Iveco", url: "https://www.youtube.com/embed/VIDEO_ID" }],
+  };
+  return <BrandPage data={data} />;
+}
