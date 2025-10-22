55c0e5bc07aa84774ad20f19b340ba464475b144
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-22 20:31:56 -0300

    Update page.tsx

--- app/marcas/volkswagen/page.tsx
+++ app/marcas/volkswagen/page.tsx
@@ -1,1 +1,17 @@
+import BrandPage, { BrandData } from "../../../components/BrandPage";
 
+export default function Page() {
+  const data: BrandData = {
+    name: "Volkswagen Meteor",
+    heroImage: "/trucks/vw-meteor.jpg",
+    logo: "/logos/volkswagen.png",
+    specs: [
+      { label: "Motor", value: "A definir" },
+      { label: "Torque", value: "A definir" },
+      { label: "Peso Bruto", value: "A definir" },
+    ],
+    gallery: ["/trucks/vw-meteor-1.jpg", "/trucks/vw-meteor-2.jpg"],
+    videos: [{ title: "VW Meteor", url: "https://www.youtube.com/embed/VIDEO_ID" }],
+  };
+  return <BrandPage data={data} />;
+}
