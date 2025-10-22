e6e46ff82d36f99368fe4c170397b673235beb42
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-22 20:32:28 -0300

    Update page.tsx

--- app/marcas/daf/page.tsx
+++ app/marcas/daf/page.tsx
@@ -1,1 +1,17 @@
+import BrandPage, { BrandData } from "../../../components/BrandPage";
 
+export default function Page() {
+  const data: BrandData = {
+    name: "DAF",
+    heroImage: "/trucks/daf.jpg",
+    logo: "/logos/daf.png",
+    specs: [
+      { label: "Motor", value: "A definir" },
+      { label: "Transmissão", value: "A definir" },
+      { label: "Cabine", value: "A definir" },
+    ],
+    gallery: ["/trucks/daf-1.jpg", "/trucks/daf-2.jpg"],
+    videos: [{ title: "DAF série XF", url: "https://www.youtube.com/embed/VIDEO_ID" }],
+  };
+  return <BrandPage data={data} />;
+}
