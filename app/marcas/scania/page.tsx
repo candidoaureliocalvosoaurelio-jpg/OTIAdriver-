88656c9ddaf64c819ed02104a1d0f1d26b5eb0f2
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-22 20:31:25 -0300

    Update page.tsx

--- app/marcas/scania/page.tsx
+++ app/marcas/scania/page.tsx
@@ -1,1 +1,17 @@
+import BrandPage, { BrandData } from "../../../components/BrandPage";
 
+export default function Page() {
+  const data: BrandData = {
+    name: "Scania",
+    heroImage: "/trucks/scania.jpg",
+    logo: "/logos/scania.png",
+    specs: [
+      { label: "Motor", value: "A definir" },
+      { label: "Torque", value: "A definir" },
+      { label: "Eixos", value: "A definir" },
+    ],
+    gallery: ["/trucks/scania-1.jpg", "/trucks/scania-2.jpg"],
+    videos: [{ title: "Scania série R", url: "https://www.youtube.com/embed/VIDEO_ID" }],
+  };
+  return <BrandPage data={data} />;
+}
