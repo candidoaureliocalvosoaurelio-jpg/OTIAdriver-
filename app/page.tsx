e78c970afa1ce0ea39979c9c173fe8b36375aa57
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-23 22:31:59 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Header from "../components/Header";
 import TruckGrid from "../components/TruckGrid";
-import TrucksGallery from "../components/TrucksGallery";
 
 export default function Home() {
   return (
