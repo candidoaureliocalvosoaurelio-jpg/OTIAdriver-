6d25c04d8e71adcc9cdcae72b0d477eb2f10e226
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-24 20:24:03 -0300

    Update electricTrucks.ts

--- data/electricTrucks.ts
+++ data/electricTrucks.ts
@@ -1,1 +1,25 @@
+// data/electricTrucks.ts
+export const electricTrucks = [
+  {
+    slug: "volvo-fh-aero-electric",
+    name: "Volvo FH Aero Electric",
+    brand: "Volvo",
+    file: "/images/electric/volvo-fh-aero-electric.png",
+  },
+  {
+    slug: "mercedes-eactros-600",
+    name: "Mercedes-Benz eActros 600",
+    brand: "Mercedes-Benz",
+    file: "/images/electric/mercedes-eactros-600.png",
+  },
+  {
+    slug: "tesla-semi",
+    name: "Tesla Semi",
+    brand: "Tesla",
+    file: "/images/electric/tesla-semi.png",
+  }
+];
 
+export function getElectricTruckBySlug(slug: string) {
+  return electricTrucks.find((t) => t.slug === slug);
+}
