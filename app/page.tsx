e130afa712e1067a29e98e4bd46494bbb9d87119
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-23 21:19:05 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,7 @@ export default function Home() {
 
       {/* CORPO PRINCIPAL */}
       <main className="text-center mt-16 px-4">
-        <h1 className="text-4xl md:text-5xl font-extrabold text-gray-900">
-          Bem-vindo à{" "}
-          <span className="bg-gradient-to-r from-blue-800 via-blue-600 to-emerald-500 bg-clip-text text-transparent">
+         <span className="bg-gradient-to-r from-blue-800 via-blue-600 to-emerald-500 bg-clip-text text-transparent">
             OTIAdriver
           </span>
         </h1>
