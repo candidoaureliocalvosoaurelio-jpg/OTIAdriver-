ae53e6e2c75fedd23c71a8d40c975d8b0b53b87b
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-24 21:48:11 -0300

    Update page.tsx

--- app/caminhoes-eletricos/[slug]/page.tsx
+++ app/caminhoes-eletricos/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { trucks, getTruckBySlug } from "../../data/trucks";
+import { trucks, getTruckBySlug } from "../../../data/trucks";
 
 type Props = { params: { slug: string } };
 
