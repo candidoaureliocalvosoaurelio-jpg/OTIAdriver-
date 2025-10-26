b3ba064680f18724878dd009139d1364dc84f2d3
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-25 21:41:24 -0300

    Update page.tsx

--- app/proposito/page.tsx
+++ app/proposito/page.tsx
@@ -1,12 +1,9 @@
-import Header from "../../components/Header"; // se o Header está em /components
 import MissaoVisaoValores from "../../components/MissaoVisaoValores"; // (ou .../components/... se você mover)
 
 export default function PropositoPage() {
   return (
-    <>
-      <Header />
-      <main className="min-h-screen bg-[#D7F1FB] py-10">
-        <MissaoVisaoValores />
+    <main className="min-h-screen bg-[#D7F1FB] py-10">
+      <MissaoVisaoValores />
       </main>
     </>
   );
