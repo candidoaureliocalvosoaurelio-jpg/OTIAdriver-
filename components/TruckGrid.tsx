91a7fc7c07cbb53e858b8b2001b6de53a27c7c5f
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-10-26 12:36:44 -0300

    Update TruckGrid.tsx

--- components/TruckGrid.tsx
+++ components/TruckGrid.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { trucks } from "../data/trucks";
 
-export default function TruckGrid() {
+export function TruckGrid() {
   return (
     <section className="py-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
