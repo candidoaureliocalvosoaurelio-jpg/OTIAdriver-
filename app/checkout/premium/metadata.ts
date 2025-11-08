5256451c911cffd01f032945bed36e02004c6689
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-11-08 16:08:19 -0300

    Update metadata.ts

--- app/checkout/premium/metadata.ts
+++ app/checkout/premium/metadata.ts
@@ -1,4 +1,6 @@
-export const metadata = {
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
   title: "Checkout Premium | OTIAdriver",
   description: "Finalize sua assinatura do plano Premium.",
 };
