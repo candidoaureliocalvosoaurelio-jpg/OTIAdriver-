2217c4b1fb3723c75cebd26b76df82ef59453f8a
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-11-09 19:55:00 -0300

    Update page.tsx

--- app/proposito/page.tsx
+++ app/proposito/page.tsx
@@ -1,9 +1,9 @@
-import MissaoVisaoValores from "../../components/MissaoVisaoValores";
+import MissaoVisaoValores from "@/components/MissaoVisaoValores";
 
 export default function PropositoPage() {
   return (
     <main className="min-h-screen bg-[#D7F1FB] py-10">
-      <div className="max-w-6xl mx-auto px-4">
+      <div className="mx-auto max-w-6xl px-4">
         <MissaoVisaoValores />
       </div>
     </main>
