cd32aff6363f5ee04912989bd09775859bac5b5c
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-11-09 20:26:44 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 export const metadata: Metadata = {
   title: "OTIAdriver",
-  description: "Conhecimento Inteligente para Motoristas",
+  description: "Conhecimento",
 };
 
 export default function RootLayout({
