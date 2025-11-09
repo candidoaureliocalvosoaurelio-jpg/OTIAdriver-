d6ce31cebcc9a2e75b3f62edbc4300e2d683f3fd
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-11-09 14:25:13 -0300

    Update page.tsx

--- app/pneus/page.tsx
+++ app/pneus/page.tsx
@@ -2,8 +2,8 @@
 
 export default function PneusPage() {
   return (
-    <main className="min-h-screen w-screen py-10 flex items-center justify-center bg-white">
-      <h1 className="text-3xl md:text-5xl font-bold text-gray-800">
+    <main className="min-h-screen w-screen py-10 flex items-center justify-center bg-gradient-to-b from-[#eef7ff] to-white">
+      <h1 className="text-3xl md:text-5xl font-extrabold text-[#0F2454]">
         Página de Pneus
       </h1>
     </main>
