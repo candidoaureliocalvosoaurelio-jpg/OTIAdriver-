b4dd44215a4e57c02d14a470961e6e80cbdee709
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-11-15 16:40:27 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
+// 1. Importa a função para habilitar o MDX
+const withMDX = require('@next/mdx')({
+  // Opção para habilitar o suporte a arquivos .md E .mdx
+  extension: /\.mdx?$/,
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+const nextConfig = {
+  // Adiciona a extensão .md e .mdx ao roteamento do Next.js
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+};
+
+// 3. Exporta a configuração final
+module.exports = withMDX(nextConfig);
