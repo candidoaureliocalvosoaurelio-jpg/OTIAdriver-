dcd8696ea07deec41560b7ce7315ec374b76b371
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-11-18 22:25:06 -0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -68,11 +68,11 @@ export default function Header() {
             stroke="currentColor"
           >
             <path
-              strokelinecap="round"
-              strokelinejoin="round"
-              strokewidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
+             strokeLinecap="round"
+             strokeLinejoin="round"
+             strokeWidth={2}
+             d="M4 6h16M4 12h16M4 18h16"
+             />
           </svg>
         </button>
       </div>
