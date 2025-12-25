94c830a02b81f6334e19e71f29030066b2eb8d3e
candidoaureliocalvosoaurelio-jpg <candidoaureliocalvosoaurelio@gmail.com>
2025-12-25 01:39:48 -0300

    Update page.tsx

--- app/entrar/page.tsx
+++ app/entrar/page.tsx
@@ -1,13 +1,7 @@
-import { Suspense } from "react";
+"use client";
+
 import EntrarClient from "./EntrarClient";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export default function EntrarPage() {
-  return (
-    <Suspense fallback={<div className="min-h-screen bg-slate-50" />}>
-      <EntrarClient />
-    </Suspense>
-  );
+export default function Page() {
+  return <EntrarClient />;
 }
